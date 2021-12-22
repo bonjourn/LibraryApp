5c51be80d12aab45f618ba39cdb1dd2ed913e520
bonjourn <shameem_18a018cs@gecwyd.ac.in>
2021-12-22 08:32:40 +0530

    update

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
 const express = require('express');
-
+const booksRouter = express.Router();
 const app = express();
 
+app.use(express.static('./public'));
+app.set('view engine','ejs');
+app.set('views',__dirname+'/src/views');
+app.use('/books',booksRouter);
+
 app.get('/',function(req,res){
-    res.send("hellow world");
+    res.render("index",
+    {
+        nav:[{link:'/books',name:'Books'},{link:'/authors',name:'Authors'}],title:'Library'
+
+    });
+});
+
+var books = [
+    {
+        title: 'Tom and Jerry',
+        author: 'joseph barbera',
+        genre: 'cartoon',
+        img: "tom.jpeg"
+
+    },
+    {
+        title: 'Harry Potter',
+        author: 'JK Rowling',
+        genre: 'Fantasy',
+        img: "harry.jpeg"
+
+    },
+    {
+        title: 'Pathummayude aadu',
+        author: 'joseph barbera',
+        genre: 'drama',
+        img: "pathu.jpeg"
+
+    }
+]
+
+booksRouter.get('/',function(req,res){
+    res.render("books",
+    {
+        nav:[{link:'/books',name:'Books'},{link:'/authors',name:'Authors'}],title:'Library',books
+
+    });
 });
 
 app.listen(5000);
+  
